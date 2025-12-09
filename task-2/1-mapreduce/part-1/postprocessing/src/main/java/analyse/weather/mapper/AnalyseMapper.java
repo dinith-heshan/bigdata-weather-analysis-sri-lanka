c8482fda903a1899e31851f6a7633d82994dc433
DinithHeshan <dinithheshansp@gmail.com>
2025-12-10 02:19:45 +0530

    feat: task2 part1 q1 added

---
+++ task-2/1-mapreduce/part-1/postprocessing/src/main/java/analyse/weather/mapper/AnalyseMapper.java
@@ -0,0 +1,61 @@
+package analyse.weather.mapper;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
+
+public class AnalyseMapper extends Mapper<LongWritable, Text, Text, Text> {
+
+    private final Map<String, String> locMap = new HashMap<>();
+
+    @Override
+    protected void setup(Context context) throws IOException {
+        URI[] cacheFiles = context.getCacheFiles();
+        if (cacheFiles != null) {
+            for (URI uri : cacheFiles) {
+                String path = uri.getPath();
+                if (path.endsWith("locationData.csv")) {
+                    try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+                        String line;
+                        while ((line = br.readLine()) != null) {
+                            if (line.toLowerCase().startsWith("location_id")) continue;
+                            String[] f = line.split(",", -1);
+                            String location_id = f[0].trim();
+                            String location = f[7].trim();
+                            locMap.put(location_id, location);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        String line = value.toString();
+        if (key.get() == 0 && line.toLowerCase().startsWith("location_id")) {
+            return;
+        }
+        String[] f = line.split(",", -1);
+
+        String location_id = f[0].trim();
+        String date = f[1].trim();
+        String temperature_2m_mean = f[5].trim();
+        String precipitation_hours = f[13].trim();
+
+        String location = locMap.getOrDefault(location_id, "UNKNOWN");
+        String month = date.split("/")[0];
+
+        Text outKey = new Text(location + "-" + month);
+        Text outVal = new Text(temperature_2m_mean + "," + precipitation_hours);
+
+        context.write(outKey, outVal);
+    }
+}
---
+++ task-2/1-mapreduce/part-1/postprocessing/src/main/java/analyse/weather/reducer/AnalyseReducer.java
@@ -0,0 +1,57 @@
+package analyse.weather.reducer;
+
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
+public class AnalyseReducer extends Reducer<Text, Text, Text, Text> {
+
+    private final Text outVal = new Text();
+
+    @Override
+    protected void reduce(Text key, Iterable<Text> values, Context context)
+            throws IOException, InterruptedException {  
+                      
+        double sum_temperature_2m_mean = 0.0;
+        int sum_precipitation_hours = 0;
+        int val_count = 0;
+
+        for (Text val : values) {
+            double temperature_2m_mean = Double.parseDouble(val.toString().split(",", -1)[0]);
+            int precipitation_hours = Integer.parseInt(val.toString().split(",", -1)[1]);
+
+            sum_temperature_2m_mean += temperature_2m_mean;
+            sum_precipitation_hours += precipitation_hours;
+
+            val_count++;
+        }
+
+        double avg_temperature_2m_mean = sum_temperature_2m_mean/val_count;
+
+        String location = key.toString().split("-", -1)[0];
+        int month = Integer.parseInt(key.toString().split("-", -1)[1]);
+
+        String monthOrdinal = getMonthOrdinal(month);
+
+        String result = String.format(
+            "%s had a total precipitation of %d hours with a mean temperature of %.1f for %s month",
+            location, sum_precipitation_hours, avg_temperature_2m_mean, monthOrdinal
+        );
+
+        outVal.set(result);
+        context.write(key, outVal);
+    }
+
+    private String getMonthOrdinal(int month) {
+        if (month > 3) {
+            return month + "th";
+        }
+        switch (month) {
+            case 1: return month + "st";
+            case 2: return month + "nd";
+            case 3: return month + "rd";
+            default: return month + "th";
+        }
+    }
+}
