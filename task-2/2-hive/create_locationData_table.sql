337e8d0810bb880154f031f2e420b0f53259995f
Dinith Heshan <104671284+DinithHeshan@users.noreply.github.com>
2025-12-11 00:04:15 +0530

    Create create_locationData_table.sql

---
+++ task-2/2-hive/create_locationData_table.sql
@@ -0,0 +1,15 @@
+CREATE EXTERNAL TABLE locationData (
+  location_id INT,
+  latitude DOUBLE,
+  longitude DOUBLE,
+  elevation INT,
+  utc_offset_seconds INT,
+  timezone STRING,
+  timezone_abbreviation INT,
+  city_name STRING
+)
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
+WITH SERDEPROPERTIES ("separatorChar" = ",")
+STORED AS TEXTFILE
+LOCATION '/data/raw/'
+TBLPROPERTIES ("skip.header.line.count"="1");
