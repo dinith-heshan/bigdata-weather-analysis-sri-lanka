4c71634e35f617c9a33471f4b4e6c049d2e96864
Dinith Heshan <104671284+DinithHeshan@users.noreply.github.com>
2025-12-12 01:52:43 +0530

    Update query_weekly_maximum_temperatures_for_hottest_months.sql

--- task-2/3-spark/part-2/query_weekly_maximum_temperatures_for_hottest_months.sql
+++ task-2/3-spark/part-2/query_weekly_maximum_temperatures_for_hottest_months.sql
@@ -2,66 +2,34 @@ WITH monthly_avg AS (
     SELECT
         year(processed_date) AS year,
         month(processed_date) AS month,
-        AVG(temperature_2m_max) AS avg_max_temp
+        AVG(temperature_2m_max) AS temperature_2m_max_avg
     FROM weatherData
     GROUP BY year(processed_date), month(processed_date)
 ),
 
 hottest_months AS (
-    SELECT *
+    SELECT year, month
     FROM (
-        SELECT
-            year,
-            month,
-            avg_max_temp,
-            ROW_NUMBER() OVER (PARTITION BY year ORDER BY avg_max_temp DESC) AS rn
+        SELECT *,
+               MAX(temperature_2m_max_avg) OVER () AS max_temp
         FROM monthly_avg
-    ) t
-    WHERE rn = 1   -- hottest month
-)
-SELECT * FROM hottest_months;
-
-
-
-
-
-WITH monthly_avg AS (
-    SELECT
-        year(processed_date) AS year,
-        month(processed_date) AS month,
-        AVG(temperature_2m_max) AS avg_max_temp
-    FROM weatherData
-    GROUP BY year(processed_date), month(processed_date)
-),
-
-hottest_months AS (
-    SELECT *
-    FROM (
-        SELECT
-            year,
-            month,
-            avg_max_temp,
-            ROW_NUMBER() OVER (PARTITION BY year ORDER BY avg_max_temp DESC) AS rn
-        FROM monthly_avg
-    ) t
-    WHERE rn = 1
+    ) AS hottest_months_with_max_temp
+    WHERE temperature_2m_max_avg = max_temp
 )
 
 SELECT
-    w.location_id,
     h.year,
     h.month,
     weekofyear(w.processed_date) AS week,
-    MAX(w.temperature_2m_max) AS weekly_max_temperature
+    MAX(w.temperature_2m_max) AS temperature_2m_max_weekly_max
 FROM weatherData w
 JOIN hottest_months h
 ON year(w.processed_date) = h.year
 AND month(w.processed_date) = h.month
 GROUP BY
-    w.location_id,
     h.year,
     h.month,
-    weekofyear(w.processed_date)
+    week
 ORDER BY
     h.year,
     h.month,
