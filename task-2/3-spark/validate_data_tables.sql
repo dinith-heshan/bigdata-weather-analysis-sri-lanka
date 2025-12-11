05051cd0e52c77d3bd45f13367ef574ec97e5cae
Dinith Heshan <104671284+DinithHeshan@users.noreply.github.com>
2025-12-11 08:12:26 +0530

    Create validate_data_tables.sql

---
+++ task-2/3-spark/validate_data_tables.sql
@@ -0,0 +1,13 @@
+SELECT *
+FROM locationData
+LIMIT 10;
+
+SELECT COUNT(*) AS total_rows
+FROM locationData;
+
+SELECT *
+FROM weatherData
+LIMIT 10;
+
+SELECT COUNT(*) AS total_rows
+FROM weatherData;
