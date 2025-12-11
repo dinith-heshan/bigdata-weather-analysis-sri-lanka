2f67a102278bb33367ef14bc101100c2addb0f51
Dinith Heshan <104671284+DinithHeshan@users.noreply.github.com>
2025-12-11 15:06:13 +0530

    Update monthly_shortwave_radiation_percentage_over_15MJ.sql

--- task-2/3-spark/part-1/monthly_shortwave_radiation_percentage_over_15MJ.sql
+++ task-2/3-spark/part-1/monthly_shortwave_radiation_percentage_over_15MJ.sql
@@ -1,1 +1,16 @@
+CREATE OR REPLACE TEMPORARY VIEW weather_with_month AS
+SELECT
+    *,
+    YEAR(processed_date) AS year,
+    MONTH(processed_date) AS month
+FROM weatherData;
+
+SELECT
+    year,
+    month,
+    SUM(CASE WHEN shortwave_radiation_sum > 15 THEN shortwave_radiation_sum ELSE 0 END) * 100.0
+        / SUM(shortwave_radiation_sum) AS percentage_above_15
+FROM weather_with_month
+GROUP BY year, month
+ORDER BY year, month;
 
